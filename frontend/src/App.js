4b44476230fafe4f1ab99c65f8159b25e9a974e9
NotOriginal333 <m.naumovych2@gmail.com>
2024-09-18 17:25:25 +0300

    fixed many issues working_version

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,14 @@
+// src/App.js
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import Register from "./Register";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn...
-        </a>
+        <h1>Welcome to the User Registration App</h1>
+        <Register />
       </header>
     </div>
   );
